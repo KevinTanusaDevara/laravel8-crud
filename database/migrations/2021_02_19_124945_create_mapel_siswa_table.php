cda28fcb18d9998c67ad53fc23f37e56a63edd73
Kevin Tanusa Devara <lionnasuki@gmail.com>
2021-02-19 20:44:26 +0700

    menambahkan table matakuliah dan nilai

---
+++ database/migrations/2021_02_19_124945_create_mapel_siswa_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMapelSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mapel_siswa', function (Blueprint $table) {
+            $table->id();
+            $table->integer('siswa_id')->legth('11');
+            $table->integer('mapel_id')->legth('11');
+            $table->integer('nilai')->legth('11');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mapel_siswa');
+    }
+}
